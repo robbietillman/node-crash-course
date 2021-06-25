48a22d45bcb280d7aea545ea9c905cf55a13d220
Robbie <robbiet@live.unc.edu>
2021-06-24 22:25:48 -0500

    made a routes folder and cut code from other files. about to cut code again to make an MVC

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const morgan = require ('morgan');
 const mongoose = require('mongoose')
-const Blog = require('./models/blog')
+const { response } = require('express');
+const { render } = require('ejs');
+const { result } = require('lodash');
+const blogRoutes = require('./routes/blogRoutes')
 
 // express app
 const app = express();
@@ -17,71 +20,15 @@ app.set('view engine', 'ejs')
 
 // middleware and static files
 app.use(express.static('public'))
-
-
+app.use(express.urlencoded({extended: true}))
 app.use(morgan('dev'))
 
-// mongoose and mongo sandbox routes
-app.get('/add-blog', (req, res) => {
-    const blog = new Blog({
-        title: "New Blog",
-        snippet: "about my new blog",
-        body: "more about my new blog"
-    })
-    blog.save()
-        .then((result) => {
-            res.send(result)
-        })
-        .catch((err) => {
-            console.log(err)
-        })
-})
-
-app.get('/all-blogs', (req,res) => {
-    Blog.find()
-        .then((result) => {
-            res.send(result)
-        })
-        .catch((err) => {
-            console.log(err)
-        })
-})
-
-app.get('/single-blog', (req, res) => {
-    Blog.findById('60ce90db5ddb4f06bc0198ea')
-        .then((result) => {
-            res.send(result)
-        })
-        .catch((err) => {
-            console.log(err)
-        })
-})
-
-
-app.use((req, res, next) => {
-    console.log('new request made:')
-    console.log('host: ', req.hostname)
-    console.log('path: ', req.path)
-    console.log('method: ', req.method)
-    next();
-});
-
-app.use((req, res, next) => {
-    console.log('in the next middleware')
-    next();
-});
-
+// routes
 app.get('/', (req, res) => {
     // have to include root: __dirname to tell function the relative path is in the cwd
     // res.sendFile('./views/index.html', {root:  __dirname})
 
-    const blogs = [
-        {title: 'Yoshi finds eggs', snippet: 'Lorem ipsum dolor sit amet consectetur'},
-        {title: 'Mario finds stars', snippet: 'Lorem ipsum dolor sit amet consectetur'},
-        {title: 'How to defeat bowser', snippet: 'Lorem ipsum dolor sit amet consectetur'},
-      ];
-    res.render('index', {title: 'Home', blogs})
-
+    res.redirect('/blogs')
 });
 
 app.get('/about', (req, res) => {
@@ -89,13 +36,12 @@ app.get('/about', (req, res) => {
     res.render('about', {title: 'About'})
 });
 
+// Blog routes
+app.use('/blogs', blogRoutes)
+
 // redirects
 app.get('/about-us', (req, res) => {
     res.redirect('/about')
-})
-
-app.get('/blogs/create', (req, res) => {
-    res.render('create', {title: 'Create a new post'})
 })
 
 
